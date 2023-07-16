cae1b62e6d610626a00c7ee8d448c51535d698cd
rajan <rajanshahi1267@gmail.com>
2023-07-16 07:54:14 -0700

    add

--- src/navbar/Nabbar.jsx
+++ src/navbar/Nabbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./navbar.css";
+import {BsFillPersonFill} from "react-icons/bs"
 
 export default function Nabbar() {
   return (
@@ -7,6 +8,21 @@ export default function Nabbar() {
       <div className="navbar-componetns">
         <div className="narbar-rightcomponents">
           <span>NKST SEC SCH</span>
+        </div>
+
+        <div className="right">
+          <div className="homebar">
+            <span className="1"></span>
+            <span className="2"></span>
+            <span className="3"></span>
+          </div>
+        </div>
+
+        <div className="rightContiiner">
+          <p>18-jul-2023 9:45:52</p>
+          <span><BsFillPersonFill/></span>
+          
+
         </div>
       </div>
     </div>
